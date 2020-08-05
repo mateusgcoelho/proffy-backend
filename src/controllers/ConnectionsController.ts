1936bf83d267c028d1a9730cd397a01c95babb6a
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-08-05 01:35:25 -0300

    Upload da primeira versão do back-end do projeto Proff pela Rocketseat.

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import db from '../database/connection'
+ 
+export default class ConnectionsController {
+    async index(req: Request, res: Response) {
+        const totalConnections = await db('connections').count('* as total')
+
+        const { total } = totalConnections[0]
+
+        return res.json({ total })
+    }
+
+    async create(req: Request, res: Response) {
+        const { user_id } = req.body
+
+        try {
+            await db('connections').insert({
+                user_id
+            })
+    
+            return res.status(201).send()
+        } catch(error) {
+            return res.status(400).json({
+                erro: 'Error when creating a new connection'
+            })
+        }
+
+    }
+}
